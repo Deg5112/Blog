544ea56683c1084481c63b1acd945edaf0f4016c
dnguyen81git <davidhuynguyen81@gmail.com>
2015-12-15 17:41:35 -0800

    changed delete to soft delete where you change the public to false to prevent it from showing, added updateblogpost a query user to select the userid to select the row to update

--- php/deleteBlogPost.php
+++ php/deleteBlogPost.php
@@ -3,7 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
 $id = (int)$_POST['blog_id'];
-$sql = "DELETE FROM blog WHERE id='$id'";
+$sql = " FROM blog WHERE id='$id'";
+$sql = "UPDATE `blog` SET `public` = FALSE, `edited` = NOW() WHERE id='$id'";
 
 if (mysqli_query($conn, $sql)){
     print("Record deleted successfully");
--- php/updateBlogPost.php
+++ php/updateBlogPost.php
@@ -6,10 +6,18 @@ $newBlog = $_POST['blog'];
 $newSummary = $_POST['summary'];
 $newTags = $_POST['tags'];
 $newPublic = $_POST['public'];
+$newUser = $_POST['user'];
+$queryUser = "SELECT users.user_id FROM users JOIN blog ON users.id = blog.user_id WHERE users.username = '$newUser'";
+$result = mysqli_query($conn, $queryUser);
+if(mysqli_affected_rows($result)>0){
+    while ($row = mysqli_fetch_assoc($result)){
+        $userId[]=$row;
+        print_r($userId);
+    }
 
-
+}
 //print_r($newInsert);
-$query = "UPDATE `blog` SET `title` = '$newTitle', `blog` = '$newBlog', `summary` = '$newSummary', `Tags` = '$newTags', `public` = '$newPublic', `edited` = NOW()";
+$query = "UPDATE `blog` SET `title` = '$newTitle', `blog` = '$newBlog', `summary` = '$newSummary', `Tags` = '$newTags', `public` = '$newPublic', `edited` = NOW() WHERE `user_id` = '$userId'";
 mysqli_query($conn, $query);
 if(mysqli_affected_rows($conn)>0){
 
