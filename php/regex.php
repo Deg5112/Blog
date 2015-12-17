f3d7bf3e03f00f7657f4ca7aefc877bb1726a79f
Nicole Fisher <ncspence4@gmail.com>
2015-12-16 17:40:10 -0800

    Added headers to registphp, connectphp, and regexphp

--- php/regex.php
+++ php/regex.php
@@ -1,6 +1,6 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 function isValidPassword($password){
     $regex = '/^(\w|[!@#$%^&*()]){7,50}/';
     return preg_match($regex,$password);
--- php/register.php
+++ php/register.php
@@ -1,6 +1,6 @@
 <?php
-//header('Access-Control-Allow-Origin: *');
-//header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
 require('connect.php');
 require('regex.php');
 
