96411f20bafc317a3ba01462bb65eb0ffa5f6031
Chelsea <chelseadeonne@gmail.com>
2015-12-16 19:35:28 -0800

    login start

--- javascript/LogInController.js
+++ javascript/LogInController.js
@@ -11,9 +11,24 @@ blog.controller('loginController', function($http, $log, loginRegisterService){
         console.log(username + password);
         self.login = {};
         loginRegisterService.loginToDb(username, password).then(function(response){
-            console.log(response);
+            // if a token property exists in response, place in browser
+            // if there is not a token, is there a response.data.data
+            if(response.data.token) {
+                localStorage.setItem("token", response.data.token);
+                console.log("token ", response.data.token);
+                return;
+
+            }
+            if (response.data.data) {
+                var datamessage = response.data.data;
+                var listItem = $('<li>').text(datamessage);
+                $('#usernameLog').append(listItem);
+                console.log("data ", response.data.data);
+            }
+            //console.log(response);
         });
     };
+
 
     self.registerUser = function(user, email, pw, confirmPw){
         //console.log(self.register);
