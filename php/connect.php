6d7c724a41cf3ec4e5ac9ee06691a0f6bd94d24c
dnguyen81git <davidhuynguyen81@gmail.com>
2015-12-16 19:36:29 -0800

    more robust delete

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'root', 'lfz_blog');
+$conn = mysqli_connect('52.35.28.205', 'nicole', 'nichole', 'lfz_blog');
 ?>
--- php/updateBlogPost.php
+++ php/updateBlogPost.php
@@ -8,9 +8,12 @@ $newTags = trim(addslashes($_POST['tags']));
 $newPublic = trim(addslashes($_POST['public']));
 $newUser = trim(addslashes($_POST['user']));
 $newBlogPost = trim(addslashes($_POST['blog_id']));
+$token = $_POST('token');
+
 
 $queryUser = "SELECT users.user_id FROM users JOIN blog ON users.id = blog.user_id WHERE users.username = '$newUser'";
 $result = mysqli_query($conn, $queryUser);
+
 if(mysqli_affected_rows($result)>0){
     while ($row = mysqli_fetch_assoc($result)){
         $userId[]=$row;
@@ -18,12 +21,18 @@ if(mysqli_affected_rows($result)>0){
     }
 
 }
-//print_r($newInsert);
+print_r($newInsert);
+$queryToken = "SELECT auth_token.auth_token FROM auth_token WHERE auth_token.user_id = $newInsert";
+if ($token == $queryToken){
+
 $query = "UPDATE `blog` SET `title` = '$newTitle', `blog` = '$newBlog', `summary` = '$newSummary', `Tags` = '$newTags', `public` = '$newPublic', `edited` = NOW() WHERE `id` = '$newBlogPost'";
 mysqli_query($conn, $query);
 if(mysqli_affected_rows($conn)>0){
 
 //    print_r($query);
+}
+}else{
+print('error, Login is invalid, please login again');
 }
 
 
